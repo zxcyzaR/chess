78c895a61941b8ede37f288377cebde855a8e38a
xyzaR <flueray@mail.ru>
2022-12-21 23:17:57 +0300

    FVVBCV

--- chess/main/urls.py
+++ chess/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path("create", views.create, name="create"),
 ]
